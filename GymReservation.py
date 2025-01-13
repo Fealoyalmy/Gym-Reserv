176f4e3ad7acfaa669066e9a55a423cf0fa29b7a
Fealoyalmy <1335691160@qq.com>
2025-01-13 11:22:42 +0800

    优化延迟生成逻辑，异常捕获功能

--- GymReservation.py
+++ GymReservation.py
@@ -12,6 +12,7 @@ import schedule
 import time
 import yaml
 import os
+import random
 from datetime import date
 
 
@@ -86,67 +87,83 @@ if __name__ == "__main__":
     def job(header, data):
         # 创建会话
         reserv = Reservation(header)
+        success = False  # 是否预约成功
+        count = 0  # 发送请求的次数
 
-        success = False
-        count = 0
-        while config["retry"] and not success and count < config["max_tries"]:
-            # 请求获取验证码图片
-            response = json.loads(reserv.requestCaptcha().text)
-            print("获取验证码:")
-            print(response["d"]["wordList"], response["d"]["secretKey"], response["d"]["token"])
-            file_content = response["d"]["originalImageBase64"]
-            secretKey = response["d"]["secretKey"]
-            token = response["d"]["token"]
-            wordList = response["d"]["wordList"]
-            img = reserv.getImageFromBase64(file_content)
+        while count == 0 or config["retry"] and not success and count < config["max_tries"]:
+            try:
+                # 请求获取验证码图片
+                response = json.loads(reserv.requestCaptcha().text)
+                print("获取验证码:")
+                print(response["d"]["wordList"], response["d"]["secretKey"], response["d"]["token"])
+                file_content = response["d"]["originalImageBase64"]
+                secretKey = response["d"]["secretKey"]
+                token = response["d"]["token"]
+                wordList = response["d"]["wordList"]
+                img = reserv.getImageFromBase64(file_content)
 
-            # 识别验证码计算点击坐标
-            Captcha.transformCaptcha("captcha.png", wordList)
-            img_base64 = Captcha.img2base64("output_image.png")
-            # print("生成图片的base64:", img_base64)
-            result = Captcha.identifyCaptcha(img_base64)
-            # 提取并转换数据
-            point = [
-                {"x": item["X坐标值"], "y": item["Y坐标值"]}
-                for item in result["data"].values()
-            ]
-            point = str(point).replace("'", '"').replace(" ", "")
-            print("识别坐标:", point)
+                # 识别验证码计算点击坐标
+                Captcha.transformCaptcha("captcha.png", wordList)
+                img_base64 = Captcha.img2base64("output_image.png")
+                # print("生成图片的base64:", img_base64)
+                result = Captcha.identifyCaptcha(img_base64)
+                # 提取并转换数据
+                point = [
+                    {"x": item["X坐标值"], "y": item["Y坐标值"]}
+                    for item in result["data"].values()
+                ]
+                point = str(point).replace("'", '"').replace(" ", "")
+                print("识别坐标:", point)
 
-            # 发送验证码验证坐标请求
-            secretKey_bytes = secretKey.encode('utf-8') # 转换密钥为二进制字节码
-            payload = {
-                "data[captchaType]": "clickWord",
-                "data[pointJson]": reserv.encyptData(point, secretKey_bytes),  # "PK9uNvWXhuAIXretztbZJEWkbzsHPr/0q40vg2mRJ8s3Hl0jW4nS3pm3mWyphlaDqgWNvvD4CWTGCwUQHq4dFVR9Vd4KhZguuY6OSucHfgA=",
-                "data[token]": token  # "d7a94245-b55e-3ebe-950b-6a3d26c99c11"
-            }
-            print("验证码坐标请求参数:\n", payload)
-            # time.sleep(1) # 延迟发送验证码坐标，避免验证太快
-            captcha_res = reserv.captchaVerification(payload)
-            print(captcha_res.text)
+                # 发送验证码验证坐标请求
+                secretKey_bytes = secretKey.encode('utf-8') # 转换密钥为二进制字节码
+                payload = {
+                    "data[captchaType]": "clickWord",
+                    "data[pointJson]": reserv.encyptData(point, secretKey_bytes),  # "PK9uNvWXhuAIXretztbZJEWkbzsHPr/0q40vg2mRJ8s3Hl0jW4nS3pm3mWyphlaDqgWNvvD4CWTGCwUQHq4dFVR9Vd4KhZguuY6OSucHfgA=",
+                    "data[token]": token  # "d7a94245-b55e-3ebe-950b-6a3d26c99c11"
+                }
+                print("验证码坐标请求参数:\n", payload)
+                sleep_time = random.uniform(1, 1.5)  # 生成随机延迟时间
+                print("延迟%f秒" % sleep_time)
+                time.sleep(sleep_time) # 延迟发送验证码坐标，避免验证太快
+                captcha_res = reserv.captchaVerification(payload)
+                print(captcha_res.text)
 
-            # 生成加密后的预约请求坐标数据
-            position_data = reserv.encyptData(token + "---" + point, secretKey_bytes)
-            payload = {
-                "resource_id": config["reservation"]["resource_id"],  # 17
-                "code": "",
-                "remarks": "",
-                "deduct_num": 0,
-                "data": str(data).replace("'", '"').replace(" ", ""),  #"[{\"date\":\"2025-01-10\",\"period\":9497,\"sub_resource_id\":29524}]",  # "[{\"date\":\"2025-01-10\",\"period\":9354,\"sub_resource_id\":28152}]"
-                "position_data[captchaVerification]": position_data  # "kwcghAemj00ZHj48m+Vrovqh/qT1KoaZPopweVKer1IGsM8s6DoDYdgvRdHr6CXKaJt4PwnUhlL68zTjyiP4th5yxsiVw/9ls7nsZfwdER0SHRORihQkjZhE6WRnPwWi07habPE4zZwzUjZDsBa6DQ=="
-            }
-            print("预约信息请求参数:\n", payload)
-            # 发送预订场地请求
-            result = reserv.sendReserv(payload)
-            print(result.text)
-            count += 1
+                # 生成加密后的预约请求坐标数据
+                position_data = reserv.encyptData(token + "---" + point, secretKey_bytes)
 
-            # 如果预约失败则换场地重试
-            if json.loads(result.text)["m"] == "操作成功":
-                success = True
-            else:
-                interval = 13
-                data[0]["sub_resource_id"] += interval  # 选择下一个场地
+                # 获取今天的日期
+                today = date.today()
+                formatted_date = today.strftime("%Y-%m-%d")  # 格式化为 "年-月-日"
+                data[0]["date"] = formatted_date  # 设置预约日期为今天
+
+                payload = {
+                    "resource_id": config["reservation"]["resource_id"],  # 17
+                    "code": "",
+                    "remarks": "",
+                    "deduct_num": 0,
+                    "data": str(data).replace("'", '"').replace(" ", ""),  # 乒乓球："[{\"date\":\"2025-01-10\",\"period\":9497,\"sub_resource_id\":29524}]"
+                    "position_data[captchaVerification]": position_data  # "kwcghAemj00ZHj48m+Vrovqh/qT1KoaZPopweVKer1IGsM8s6DoDYdgvRdHr6CXKaJt4PwnUhlL68zTjyiP4th5yxsiVw/9ls7nsZfwdER0SHRORihQkjZhE6WRnPwWi07habPE4zZwzUjZDsBa6DQ=="
+                }
+                print("预约信息请求参数:\n", payload)
+                # 发送预订场地请求
+                time.sleep(0.4) # 延迟发送请求，避免验证太快
+                result = reserv.sendReserv(payload)
+                print(result.text)
+
+                # 如果预约失败则换场地重试
+                if json.loads(result.text)["m"] == "操作成功":
+                    success = True
+                else:
+                    interval = 13
+                    data[0]["sub_resource_id"] += interval  # 选择下一个场地
+
+            except Exception as e:
+                print("发生了错误:", e)
+                print(response)
+                time.sleep(1)
+            finally:
+                count += 1
 
 
     # 读取配置
@@ -154,45 +171,24 @@ if __name__ == "__main__":
     with open(config_path, "r") as file:
         config = yaml.safe_load(file)
     # print(config)
-    # job()
+    for i in range(len(config["reservation"]["data"])):
+        job(config["header"][i], [config["reservation"]["data"][i]])
 
     # 设置定时任务
     if config["schedule"]["use"]:
-        # schedule.every().day.at("13:35:59").do(job1)  # 每天 13:35:59 执行
-        # schedule.every().monday.at("09:00").do(job1)  # 每周一 09:00 执行
-        # schedule.every(5).seconds.do(job1)  # 每 5 秒执行一次
         if config["schedule"]["day"] == None:
             print("Everyday Schedule!")
-            # 获取今天的日期
-            today = date.today()
-            formatted_date = today.strftime("%Y-%m-%d")  # 格式化为 "年-月-日"
-            config["reservation"]["data"]["date"] = formatted_date # 设置预约日期为今天
-
-            print("账号1")
-            header1 = {
-                "user-agent": config["header"]["user-agent"],
-                "referer": config["header"]["referer"],
-                "Cookie": config["header"]["Cookie"][0]
-            }
-            data1 = [{
-                "date": formatted_date,
-                "period": config["reservation"]["data"]["period"][0],
-                "sub_resource_id": config["reservation"]["data"]["sub_resource_id"][0]
-            }]
-            schedule.every().day.at(config["schedule"]["time"]).do(job, header=header1, data=data1)
-
-            print("账号2")
-            header2 = {
-                "user-agent": config["header"]["user-agent"],
-                "referer": config["header"]["referer"],
-                "Cookie": config["header"]["Cookie"][1]
-            }
-            data2 = [{
-                "date": formatted_date,
-                "period": config["reservation"]["data"]["period"][1],
-                "sub_resource_id": config["reservation"]["data"]["sub_resource_id"][1]
-            }]
-            schedule.every().day.at(config["schedule"]["time"]).do(job, header=header2, data=data2)
+            # 遍历时间段，使用不同账号Cookie进行不同的预约
+            for i in range(len(config["reservation"]["data"])):
+                header = config["header"][i]
+                data = [config["reservation"]["data"][i]]
+                schedule.every().day.at(config["schedule"]["time"]).do(job, header=header, data=data)
+                print("(%d) 账号%s 创建定时任务: %s  period=%d sub_resource_id=%d" % (i, config["header"][i]["Cookie"][9:15], config["schedule"]["time"],
+                                                            config["reservation"]["data"][i]["period"], 
+                                                            config["reservation"]["data"][i]["sub_resource_id"]))
+                # schedule.every().day.at("13:35:59").do(job1)  # 每天 13:35:59 执行
+                # schedule.every().monday.at("09:00").do(job1)  # 每周一 09:00 执行
+                # schedule.every(5).seconds.do(job1)  # 每 5 秒执行一次
 
         # 循环运行调度器
         while True:
@@ -201,7 +197,7 @@ if __name__ == "__main__":
 
 
     
-    # 每天13个时间段（8:00-21:00）
+    # 每天13个时间段（8:00-21:00），场地号是根据按钮递增的
     # requestBody = [
     #     {"date":"2025-01-05","period":9354,"sub_resource_id":28152},  # 3号 18-19
     #     {"date":"2025-01-05","period":9350,"sub_resource_id":28148},  # 2号 14-15
@@ -209,29 +205,23 @@ if __name__ == "__main__":
     #     {"date":"2025-01-05","period":9347,"sub_resource_id":28197}   # 6号 11-12
     # ]
 
-    # responseBody = { # 验证码响应体示例
-        #     "e": 0,
-        #     "m": "操作成功",
-        #     "d": {
-        #         "originalImageBase64": "",
-        #         "secretKey": "g1lwhvs011dvly0x",
-        #         "token": "1a4cc4e2-84c7-6088-8c54-8d45c0f2c93d",
-        #         "wordList": [
-        #             "蜡",
-        #             "箍",
-        #             "松",
-        #             "卢"
-        #         ]
-        #     }
-        # }
+    """
+    验证码响应体示例
+    """
+    # responseBody = {
+    #     "e": 0,
+    #     "m": "操作成功",
+    #     "d": {
+    #         "originalImageBase64": "",
+    #         "secretKey": "g1lwhvs011dvly0x",
+    #         "token": "1a4cc4e2-84c7-6088-8c54-8d45c0f2c93d",
+    #         "wordList": [
+    #             "蜡",
+    #             "箍",
+    #             "松",
+    #             "卢"
+    #         ]
+    #     }
+    # }
 
-    # 读取base64编码文本获取图像
-    # file_path = 'img_b64.txt'
-    # with open(file_path, 'r', encoding='utf-8') as file:
-    #     file_content = file.read()
-    # img_bit = reserv.normalizeImage(img)
-    # cnt = reserv.findContour(img_bit)
-    # ret, point = reserv.extractCharContour(img_bit, reserv.aspectRatio(cnt))
-    # print(ret)
-    # print(point)
 
--- test.py
+++ test.py
@@ -1,14 +1,81 @@
+import time
 import yaml
 import os
-from datetime import date
-
-# 读取配置
-config_path = os.getenv("CONFIG_PATH", "config.yaml")
-with open(config_path, "r") as file:
-    config = yaml.safe_load(file)
-
-print(str(config["reservation"]["data"]))
+import schedule
+from datetime import date, datetime
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 
+
+
+# print(str(config["reservation"]["data"]))
+
+
+# for i in range(len(config["reservation"]["data"])):
+#     print("账号%d 创建定时任务" % i)
+#     header = config["header"][i]
+#     data = [config["reservation"]["data"][i]]
+#     print(header)
+#     print(data)
+
+# 配置文件路径
+CONFIG_FILE = "config copy.yaml"
+
+# 定义任务
+def job1():
+    print("任务 1 执行")
+
+def job2():
+    print("任务 2 执行")
+
+def job3():
+    print("任务 3 执行")
+
+
+def load_config():
+    # 读取配置
+    config_path = os.getenv("CONFIG_PATH", CONFIG_FILE)
+    with open(config_path, "r") as file:
+        config = yaml.safe_load(file)
+        return config
+
+
+# 监听文件变化的处理器
+class ConfigChangeHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.src_path.endswith(CONFIG_FILE):
+            print("检测到配置文件修改")
+            config = load_config()
+            schedule.every().day.at(config["schedule"]["time"]).do(job3)
+
+
+if __name__ == "__main__":
+    # 创建任务
+    # schedule.every(10).seconds.do(job1)
+    # schedule.every(5).seconds.do(job2)
+
+    config = load_config()
+    schedule.every().day.at(config["schedule"]["time"]).do(job3)
+
+    # 创建监听器
+    event_handler = ConfigChangeHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path=".", recursive=False)
+    observer.start()
+
+    try:
+        # 查看已创建的任务
+        while True:
+            schedule.run_pending()
+            now = datetime.now()
+            formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
+            print(formatted_time + " 当前已创建的任务:")
+            for job in schedule.jobs:
+                print(f"任务函数: {job.job_func.__name__}, 下次运行时间: {job.next_run}")
+            time.sleep(5)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
 
